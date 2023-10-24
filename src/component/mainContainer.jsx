963bf7ec9576e822a6841648cc3ccf5c9dbd4236
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-10-25 02:58:56 +0530

    add sign up page

--- src/component/mainContainer.jsx
+++ src/component/mainContainer.jsx
@@ -1,28 +1,32 @@
 import React, { useState } from "react";
 import LoginForm from "./loginForm";
 import SignUpForm from "./signUp";
+import loginImage from "../assets/login.png";
+import signUpImage from "../assets/sign-up.png";
 
 const MainContainer = () => {
   const [showSignUp, setShowSignUp] = useState(false);
 
-  function handleShowSignUp() {
-    setShowSignUp(true);
+  function handleToggleForm() {
+    setShowSignUp(!showSignUp);
   }
 
   return (
     <div className="container">
-
-
       <div className="loginContainer left">
-        <img src="../src/assets/login.png" className="loginImage" alt="Login" />
+        <img
+          src={showSignUp ? signUpImage : loginImage}
+          className="loginImage"
+          alt={showSignUp ? "Sign Up" : "Login"}
+        />
       </div>
 
       {showSignUp ? <SignUpForm /> : <LoginForm />}
 
       <div className="signUp">
-        <span className="blue light">Forgot Password ? </span>
-        <a className="blue" onClick={handleShowSignUp}>
-          Sign up here
+        <span className="blue light">Forgot Password ?</span>
+        <a className="blue" onClick={handleToggleForm}>
+          {showSignUp ? "Log in" : "Sign up here"}
         </a>
       </div>
     </div>
