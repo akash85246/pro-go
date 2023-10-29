4aaff5694c83c020f9a33f952d344248894e2ef5
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-10-29 14:27:55 +0530

    fixed minor bugs

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pro-go/",
+  base: "/",
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-toastify"], 
+      external: ["react-toastify"],
     },
   },
 });
