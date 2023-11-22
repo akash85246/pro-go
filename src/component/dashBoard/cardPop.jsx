72bc4eff2e7bad21b92432edcbcd5e2c4af166ef
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-11-22 20:00:11 +0530

    fixed some minor bugs

--- src/component/dashBoard/cardPop.jsx
+++ src/component/dashBoard/cardPop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios"; // Import Axios
+import axios from "axios";
 import "./cardPop.css";
 import { useAuth } from "../utils/authContext";
 
@@ -7,50 +7,60 @@ export default function CardPop(props) {
   const [cardData, setCardData] = useState(null);
   const [error, setError] = useState(null);
   const { authToken, setAuthToken } = useAuth();
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    // Define the API endpoint
     const apiUrl = `https://pro-go.onrender.com/api/list/${props.listId}/cards`;
 
-    // Define the headers for the request
     const headers = {
       "Content-Type": "application/json",
-      "auth-token": authToken, // Replace with your actual auth token
+      "auth-token": authToken,
     };
 
-    // Make the API request using Axios
     axios
       .get(apiUrl, { headers })
       .then((response) => {
-        const data = response.data;
-
-        console.log(data);
-        if (data.success) {
-          setCardData(data);
+        if (response.data.success) {
+          setCardData(response.data.data);
         } else {
-          setError(data.message);
+          setError(response.data.message);
         }
       })
       .catch((error) => {
         setError("An error occurred while fetching data.");
         console.error("Error fetching data:", error);
       });
-  }, []);
+  }, [authToken, props.listId]); // Added dependencies to useEffect
+
   return (
     <>
       <div className="cardPopContainer">
-        <h2>{props.heading}</h2>
-        <h1>List ID: {props.listId}</h1>
+        <h1>Added Cards</h1>
         {error ? (
           <p>Error: {error}</p>
         ) : (
           cardData && (
-            <div>
-              <div>
-                <h1>card Title:cardData.cardTitle</h1>
-                <h3>Description: {cardData.description}</h3>
+            <div className="popCardList">
+              {cardData.cards.map((card) => (
+                <div key={card._id} className="popCard">
+                  <p>Card Title: {card.name}</p>
+
+                  {card.description && (
+                    <>
+                      <p>Description:</p>
+                      <textarea
+                        className="popDescription"
+                        value={card.description}
+                        readOnly
+                      ></textarea>
+                    </>
+                  )}
+                  {/* <p>Card ID: {card._id}</p> */}
+                </div>
+              ))}
+              <div className="popupbButtons">
                 <button onClick={props.onClose}>Close</button>
-                {/* <button onClick={props.onDeleteList}>Delete list</button> */}
+                <button onClick={props.onDeleteList}>Delete list</button>
               </div>
             </div>
           )
